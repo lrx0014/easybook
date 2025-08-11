b3005eb620bd5aae14cdbbef905131ac4e58c3af
lrx0014 <lrx0014@hotmail.com>
2025-08-12 01:20:26 +0800

    init pom.xml

---
+++ server/admin-core/src/main/java/com/ryan/easybook/admin/core/AdminCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.easybook.admin.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminCoreApplication.class, args);
+	}
+
+}
